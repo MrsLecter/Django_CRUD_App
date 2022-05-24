159df8fce315977b91fa279085b6456f945341e2
MrsLecter <MissisLecter@protonmail.com>
2022-05-24 11:08:44 +0300

    add: method delete

--- employees/urls.py
+++ employees/urls.py
@@ -6,8 +6,11 @@ urlpatterns = [
     path('employees/add', views.add_employee, name='employee_add'),
     path('employee/<str:pk>/', views.employee, name='employee'),
     path('employee/<str:pk>/put', views.update_employee, name='employee_update'),
+    path('employee/<str:pk>/delete', views.delete_employee, name='employee_delete'),
 
     path('departments/', views.departments, name='departments'),
     path('department/<str:pk>/', views.department, name='department'),
+    path('department/<str:pk>/put', views.update_department, name='department_update'),
+    path('department/<str:pk>/delete', views.delete_department, name='department_delete'),
     path('departments/add/', views.add_department, name='department_add'),
 ]
--- employees/views.py
+++ employees/views.py
@@ -4,11 +4,11 @@ from .form import EmployeeForm, DepartmentForm
 
 def employees(request):
     data = Employee.objects.all()
-    return render(request, 'employees.html', {'data':data})
+    return render(request, 'employee/employees.html', {'data':data})
 
 def employee(request, pk):
     current_employee = Employee.objects.get(name=pk)
-    return render(request, 'employee.html', {'data': current_employee})
+    return render(request, 'employee/employee.html', {'data': current_employee})
 
 def add_employee(request):
     form = EmployeeForm()
@@ -31,15 +31,24 @@ def update_employee(request, pk):
     context = {'form': form}
     return render(request, 'modify_form.html', context)
 
+def delete_employee(request, pk):
+    employee = Employee.objects.get(name=pk)
+    if request.method == 'POST':
+        employee.delete()
+        return redirect('employees')
+    context = {'object': employee}
+    return render(request, 'emplayee/delete_form.html', context)
+
+
 
 def departments(request):
     data = Deparment.objects.all()
-    return render(request, 'departments.html', {'data':data})
+    return render(request, 'department/departments.html', {'data':data})
 
 def department(request, pk):
     department_name = Deparment.objects.get(id=pk)
     employees = Employee.objects.filter(department=pk)
-    return render(request, 'department.html', {'department': department_name, 'employee': employees})
+    return render(request, 'department/department.html', {'department': department_name, 'employee': employees})
 
 def add_department(request):
     form = DepartmentForm()
@@ -50,3 +59,22 @@ def add_department(request):
         return redirect('departments')
     context = {'form': form}
     return render(request, 'modify_form.html', context)
+
+def update_department(request, pk):
+    current_department = Deparment.objects.get(id=pk)
+    form = DepartmentForm()
+    if request.method == 'POST':
+        form = DepartmentForm(request.POST, instance=current_department)
+        if form.is_valid():   
+            form.save()
+        return redirect('departments')
+    context = {'form': form}
+    return render(request, 'modify_form.html', context)
+
+def delete_department(request, pk):
+    department = Deparment.objects.get(id=pk)
+    if request.method == 'POST':
+        department.delete()
+        return redirect('departments')
+    context = {'object': department}
+    return render(request, 'department/delete_form.html', context)
